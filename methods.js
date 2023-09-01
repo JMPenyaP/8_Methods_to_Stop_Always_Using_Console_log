f97ea15717efba41bf4ebbc495f858cfac2b67b6
JosePenya <josemariapep9@gmail.com>
2023-09-01 17:48:57 -0500

    8 methods to stop always using console.log

---
+++ methods.js
@@ -0,0 +1,33 @@
+// Muestra el número de veces que se ha llamado a un método
+console.count();
+
+// Un condicional, si es falso, muuestra un error
+// console.assert(e!===0,"El divisor no puede ser cero");
+console.assert();
+
+// Muestra datos en forma de tabla
+console.table();
+
+// Mide el tiempo que tarda en ejecutarse una operación entre ambos métodos
+console.time();
+console.timeEnd();
+
+// Para mostrar advertencias
+console.warn();
+
+// Para errores críticos
+console.error();
+
+// Muestra un listado de las propieddes de un objeto
+/*Object
+    name: "Dana"
+    age: 20
+[[Prototype]]:Object */
+console.dir();
+
+// Muestra la secuencia de llamadas a funciones hasta donde se ejecutó
+/*Traza de la pila de llamadas:
+functionB   @VM1117:8
+functionA   @VM1117:4
+(anonymous) @VM1117:11 */
+console.trace();
